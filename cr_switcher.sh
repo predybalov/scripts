1cf26775f14259e2ec003f6225d92fb7bbbbc909
predybalov <79003995+predybalov@users.noreply.github.com>
2022-12-13 14:52:04 +0300

    Create cr_switcher.sh

---
+++ cr_switcher.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+registry=$1
+keydir="/home/gelios/Settings/yandex_container_registries_configs"
+
+GREEN="\e[32m"
+RED="\e[31m"
+NC="\e[0m"
+BOLD="\e[1m"
+
+case "$registry" in
+    "<one>"    ) cat ${keydir}/${registry}.json | docker login --username json_key --password-stdin cr.yandex && \
+                      echo -e "to ${GREEN}${BOLD}${registry}${NC} registry";;
+    "<two>"    ) cat ${registry}.json | docker login --username json_key --password-stdin cr.yandex && \
+                      echo -e "to ${GREEN}${BOLD}${registry}${NC} registry";;
+    "<three>"  ) cat ${registry}.json | docker login --username json_key --password-stdin cr.yandex && \
+                      echo -e "to ${GREEN}${BOLD}${registry}${NC} registry";;
+    *          ) echo -e "${RED}${BOLD}Unknown registry${NC}"
+esac
