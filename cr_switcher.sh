da46a5e77c7d7567a7ca9176f55eaf581d9a68b5
predybalov <79003995+predybalov@users.noreply.github.com>
2022-12-14 17:17:52 +0300

    Update cr_switcher.sh

--- cr_switcher.sh
+++ cr_switcher.sh
@@ -11,9 +11,9 @@ BOLD="\e[1m"
 case "$registry" in
     "<one>"    ) cat ${keydir}/${registry}.json | docker login --username json_key --password-stdin cr.yandex && \
                       echo -e "to ${GREEN}${BOLD}${registry}${NC} registry";;
-    "<two>"    ) cat ${registry}.json | docker login --username json_key --password-stdin cr.yandex && \
+    "<two>"    ) cat ${keydir}/${registry}.json | docker login --username json_key --password-stdin cr.yandex && \
                       echo -e "to ${GREEN}${BOLD}${registry}${NC} registry";;
-    "<three>"  ) cat ${registry}.json | docker login --username json_key --password-stdin cr.yandex && \
+    "<three>"  ) cat ${keydir}/${registry}.json | docker login --username json_key --password-stdin cr.yandex && \
                       echo -e "to ${GREEN}${BOLD}${registry}${NC} registry";;
     *          ) echo -e "${RED}${BOLD}Unknown registry${NC}"
 esac
